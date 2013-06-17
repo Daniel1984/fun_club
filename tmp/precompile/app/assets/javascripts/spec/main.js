6122669a999a1d5d73691c46114f3663339d7f3c
daiel1984 <daniel.stenger@yahoo.com>
2013-06-18 00:51:50 +0300

    adding tests and post join

--- tmp/precompile/app/assets/javascripts/spec/main.js
+++
@@ -1,8 +0,0 @@
-describe("Test", function() {
-  require(['views/join_event/index_view'], function(View) {
-    return console.log(View);
-  });
-  return it("should return true", function() {
-    return expect(2 + 2).toEqual(5);
-  });
-});
